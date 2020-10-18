d7d47515f6966a2f5efcd00dea7692db47e93324
Hugo Hoffmann <hugohoffmann04@gmail.com>
2020-10-18 20:47:20 -0300

    chore: add routeEmpresa no app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { FuncionarioRoutes } from "./routes/funcionario";
+import { EmpresaRoutes } from "./routes/empresa";
 
 class App {
   public app: express.Application;
   public routesFuncionario: FuncionarioRoutes = new FuncionarioRoutes();
+  public routesEmpresa: EmpresaRoutes = new EmpresaRoutes();
 
   constructor() {
     this.app = express();
     this.config();
     this.routesFuncionario.routes(this.app);
+    this.routesEmpresa.routes(this.app);
   }
 
   private config(): void {
